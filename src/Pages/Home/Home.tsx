8c5786b84f46bcb2ffb76e8f078f182b41e97fe7
Rohit Guati <rawheat.gulati@hotmail.com>
2021-06-09 12:57:06 +0530

    Add Home UI

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,31 +1,37 @@
 import { quizDb } from "../../data";
 import { useQuiz } from "../../Contexts";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export function Home() {
-  const navigate = useNavigate();
   const { dispatch } = useQuiz();
   return (
     <>
       <div className="main-height my-auto flex flex-col items-center justify-center  dark:text-white dark:bg-gray-800">
-        <h1 className="text-center text-8xl text-transparent bg-clip-text bg-gradient-to-r from-green-300 to-green-400 font-sans font-bold">
-          NFT
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
-            ask
-          </span>
-        </h1>
-        <h3>Select the quiz</h3>
-        <div className="flex flex-col">
+        <div className="pb-24">
+          <h1 className="text-center text-8xl text-transparent bg-clip-text bg-gradient-to-r from-green-300 to-green-400 font-sans font-bold">
+            NFT
+            <span className="animate-pulse text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
+              ask
+            </span>
+          </h1>
+          <h2 className="text-main text-xl">
+            Are you a NFT enthusiast? How about we ask a few question and check
+            your love for them ♥.
+          </h2>
+        </div>
+        <h3 className="text-main mb-10 text-2xl">Select the quiz</h3>
+        <div className="flex flex-col items-center">
           {quizDb.quizes.map((quiz) => (
-            <button
-              className="border-2 p-2 m-2 rounded-xl border-gray-400 text-xl text-gray-500 hover:bg-gradient-to-r from-green-400 to-blue-500"
-              onClick={() => dispatch({ type: "LOAD_QUIZ", payload: quiz })}
-            >
-              {quiz.name}
-            </button>
+            <Link to="/">
+              <button
+                className="border-2 p-2 m-2 rounded-xl border-gray-400 dark:border-gray-100 text-xl text-main hover:border-l-2"
+                onClick={() => dispatch({ type: "LOAD_QUIZ", payload: quiz })}
+              >
+                {quiz.name}
+              </button>
+            </Link>
           ))}
         </div>
-        <button onClick={() => navigate("/score")}>Score</button>
       </div>
     </>
   );
